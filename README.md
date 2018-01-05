1983701f8a2a736d9622d286aa333c6ff386b8da
Berkant Korkmaz <berkantk3@gmail.com>
2018-01-06 01:35:07 +0300

    Initial v1.0

---
+++ README.md
@@ -0,0 +1,11 @@
+# yound
+An Android app which downloads mp3 from YouTube
+
+------
+
+## Usage
+* Open YouTube
+* Find the music/sound whatever you want to download
+* Click on share
+* Choose Yound
+And that's all. You'll be redirected to download page. Choose which quality you want to have there.
---
+++ app/src/main/java/io/github/berkantkz/yound/MainActivity.java
@@ -0,0 +1,26 @@
+package io.github.berkantkz.yound;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    static String baseURL = "https://youtubemp3api.com/@api/button/mp3/";
+    static String fullURL;
+    String receivedText;
+    Intent receivedIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        receivedIntent = getIntent();
+        receivedText = receivedIntent.getStringExtra(Intent.EXTRA_TEXT);
+        fullURL = baseURL + receivedText.replace("https://","").replace("http://","").replace("youtu.be/","").replace("youtube.com/","");
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(fullURL));
+        startActivity(i);
+        finish();
+    }
+}
